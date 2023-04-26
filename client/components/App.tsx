f8b3668f88f33e3d94888c2147c8958b94d9639c
Ella Robinson <ellaroosmarijn@gmail.com>
2023-04-26 23:17:07 +1200

    feat(client): add answer page route to the App.tsx component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,31 +1,57 @@
 import { Routes, Route } from 'react-router-dom'
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+  AppShell,
+} from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
+import { useHotkeys, useLocalStorage } from '@mantine/hooks'
 
+import myTheme from '../theme'
 import AnswerPage from './AnswerPage'
+import Header from './Header'
+import PageFooter from './PageFooter'
 
 function App() {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'light',
+    getInitialValueInEffect: true,
+  })
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]])
+
   return (
-    <div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <h1>App</h1>
-              <p>React development has begun!</p>
-            </>
-          }
-        />
-        <Route path="/answer" element={<AnswerPage />} />
-        <Route
-          path="*"
-          element={
-            <>
-              <h1>404</h1>Page not found
-            </>
-          }
-        />
-      </Routes>
-    </div>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ ...myTheme, colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <AppShell fixed header={<Header />}>
+          <Notifications />
+          <Routes>
+            <Route path="/answer" element={<AnswerPage />} />
+            <Route
+              path="*"
+              element={
+                <>
+                  <h1>404</h1>Page not found
+                </>
+              }
+            />
+          </Routes>
+          <PageFooter />
+        </AppShell>
+      </MantineProvider>
+    </ColorSchemeProvider>
   )
 }
 
