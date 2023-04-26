32f89eb37d9acc8f8532c3f700bb6c0007f0f5fd
Ella Robinson <ellaroosmarijn@gmail.com>
2023-04-26 23:41:29 +1200

    feat(client): add api for fetching the viewpoint and getting the topic

--- client/api/index.ts
+++ client/api/index.ts
@@ -1,9 +1,21 @@
 import axios from 'axios'
 
-import { Answer, Score } from '../../shared/types'
+import { Viewpoint, Answer, Score, CoreReducer } from '../../shared/types'
 
 export function scoreAnswerApi(answer: Answer) {
   return axios
     .post('/api/v1/answer/score', answer)
     .then((response) => response.data as Score)
+}
+
+export function viewpointApi(viewpoint: Viewpoint) {
+  return axios
+    .post('/api/v1/ideology', { viewpoint })
+    .then((response) => response.data as Viewpoint)
+}
+
+export function topicApi() {
+  return axios
+    .get('/api/v1/topic')
+    .then((response) => response.data as CoreReducer)
 }
